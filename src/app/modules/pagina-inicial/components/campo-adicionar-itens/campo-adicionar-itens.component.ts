7ec2ce3dc09a27732ed001d44f85697abd884686
Danilo Silva <danilo.silva@msn.com>
2023-01-13 00:05:30 -0300

    Estilização do Campo de Entrada

--- src/app/modules/pagina-inicial/components/campo-adicionar-itens/campo-adicionar-itens.component.ts
+++ src/app/modules/pagina-inicial/components/campo-adicionar-itens/campo-adicionar-itens.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-campo-adicionar-itens',
   templateUrl: './campo-adicionar-itens.component.html',
-  styleUrls: ['./campo-adicionar-itens.component.scss']
+  styleUrls: ['./campo-adicionar-itens.component.scss'],
 })
 export class CampoAdicionarItensComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
