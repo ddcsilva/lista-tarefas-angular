4c665c8cd3b3d996393692fe68ae9e920d2a16c9
Danilo Silva <danilo.silva@msn.com>
2023-01-13 00:20:55 -0300

    Adição de itens na lista

--- src/app/modules/pagina-inicial/components/lista-tarefas/lista-tarefas.component.ts
+++ src/app/modules/pagina-inicial/components/lista-tarefas/lista-tarefas.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interface
+import { ListaTarefas } from '../../model/lista-tarefas';
+
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
-  styleUrls: ['./lista-tarefas.component.scss']
+  styleUrls: ['./lista-tarefas.component.scss'],
 })
 export class ListaTarefasComponent implements OnInit {
+  public listaTarefas: Array<ListaTarefas> = [
+    { tarefa: 'Minha nova Tarefa', finalizado: true },
+    { tarefa: 'Minha nova Tarefa 2', finalizado: false },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
